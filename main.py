c030796ad3d4af92f0cefb50159d5b60a4c76180
iodunsi <io6@njit.edu>
2024-07-16 21:31:27 -0400

    adding logic

---
+++ main.py
@@ -0,0 +1,1 @@
+print("HW 8 - Complete!")
---
+++ tests/start_test.py
@@ -0,0 +1,6 @@
+"""This file will test if start is functioning properly."""
+from app import start
+
+def test_start():
+    """Testing the Start"""
+    assert start() == True
